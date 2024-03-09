c3003fc117b08a7c760802d4a96e99108c903332
aymen sammoud <aymensamoud3@gmail.com>
2024-03-09 13:44:38 +0100

    p

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
-import { RouterProvider } from 'react-router-dom'
+import { RouterProvider, BrowserRouter, Router, Route } from 'react-router-dom'
 import router from './router'
 import { ContextProvider } from './contexts/ContextProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+
     <ContextProvider>
     <RouterProvider router={router} />
     </ContextProvider>
+ 
+    
   </React.StrictMode>,
 )
--- src/router.jsx
+++ src/router.jsx
@@ -1,7 +1,6 @@
 import { Navigate, createBrowserRouter } from "react-router-dom";
 import Login from "./views/Login";
 import SignUp from "./views/SignUp";
-import Users from "./views/Users";
 import NotFound from "./views/NotFound";
 import DefaultLayout from "./components/DefaultLayout";
 import GuestLayout from "./components/GuestLayout";
